f29b8e9a1d233904c5d7b0574d331888dc6180f9
Supun Ranatunge <supunranatunge7@gmail.com>
2018-07-29 14:41:56 +0530

    heroku hosting

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname,'Public')));
 app.use(passport2.initialize());
 app.use(passport2.session());
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
